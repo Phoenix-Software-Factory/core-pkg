56e64179cc1dd7855f459f39ec3aba2487e85897
Alejandro Hernandez <alephoenix1985@gmail.com>
2021-10-24 23:38:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export fetchHelper from './fetch.helper.js';
 export pwaHelper from './pwa.helper.js';
 export domHelper from './pwa.helper.js';
 export jsHelper from './js.helper.js';
+export timeHelper from './time.helper.js';
