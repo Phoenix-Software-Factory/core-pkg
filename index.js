9dfb5e193f406a6793853545e7130316993f0db9
Alejandro Hernandez <alephoenix1985@gmail.com>
2021-10-24 23:35:32 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+export dbHelper from './db.helper.js';
+export fetchHelper from './fetch.helper.js';
+export pwaHelper from './pwa.helper.js';
+export domHelper from './pwa.helper.js';
+export jsHelper from './js.helper.js';
