aa28c27e30d078a656a216a51f9ea4ad93c5fbe5
Alejandro Hernandez <alephoenix1985@gmail.com>
2021-10-25 02:20:54 +0200

    asd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-export dbHelper from './db.helper.js';
-export fetchHelper from './fetch.helper.js';
-export pwaHelper from './pwa.helper.js';
-export domHelper from './pwa.helper.js';
-export jsHelper from './js.helper.js';
-export timeHelper from './time.helper.js';
+export * as dbHelper from './db.helper.js';
+export * as fetchHelper from './fetch.helper.js';
+export * as pwaHelper from './pwa.helper.js';
+export * as domHelper from './pwa.helper.js';
+export * as jsHelper from './js.helper.js';
+export * as timeHelper from './time.helper.js';
